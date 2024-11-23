0611aa5e74269fa599286f69005d8009e0ebf1f7
ispamer98 <ispamer98@gmail.com>
2024-11-23 16:00:33 +0100

    Front

--- rxconfig.py
+++ rxconfig.py
@@ -6,8 +6,7 @@ config = rx.Config(
     app_name="HeroHistory",
     icon="/favicon.ico",
       # Usa el puerto correcto si lo estás personalizando.
-    api_url="http://localhost:8000",
-    cors_allow_origins=["https://herohistory.reflex.run",
-                        "local://localhost:3000"],
+    api_url="https://herohistory.onrender.com",
 )
 
+gi
