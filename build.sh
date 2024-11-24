3e4b38c3cb15dfad930f2dc55b57ed147c0bb7c6
ispamer98 <ispamer98@gmail.com>
2024-11-24 15:37:09 +0100

    remote deploy

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf public
+rm -rf frontend
 reflex init
 API_URL=https://https://herohistory-back.onrender.com reflex export --frontend-only
 unzip frontend.zip -d public
