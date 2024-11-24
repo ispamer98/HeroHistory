d1931444da25f1957179bc865aa0292b6cc45355
ispamer98 <ispamer98@gmail.com>
2024-11-24 16:46:56 +0100

    errores

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf frontend
 reflex init
-API_URL=https://https://herohistory-back.onrender.com reflex export --frontend-only
+API_URL=https://herohistory-back.onrender.com/ reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
