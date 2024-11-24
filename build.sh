450c8857fe7d2a7b8a6d3dd05e800a450f44a067
ispamer98 <ispamer98@gmail.com>
2024-11-24 16:48:56 +0100

    errores

--- build.sh
+++ build.sh
@@ -1,11 +1,30 @@
+#!/bin/bash
+
+# Cambiar al directorio del proyecto
 cd herohistory
+
+# Crear y activar el entorno virtual
 python -m venv .venv
 source .venv/bin/activate
+
+# Actualizar pip e instalar las dependencias
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Eliminar cualquier carpeta de frontend existente
 rm -rf frontend
+
+# Inicializar reflex (esto solo es necesario si es un proyecto nuevo)
 reflex init
-API_URL=https://herohistory-back.onrender.com/ reflex export --frontend-only
+
+# Establecer la URL de la API y exportar solo el frontend
+API_URL=https://herohistory-back.onrender.com reflex export --frontend-only
+
+# Descomprimir el archivo frontend.zip en la carpeta public
 unzip frontend.zip -d public
+
+# Eliminar el archivo frontend.zip después de descomprimir
 rm -f frontend.zip
+
+# Desactivar el entorno virtual
 deactivate
